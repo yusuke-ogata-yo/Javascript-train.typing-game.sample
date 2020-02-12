a41c06dedd0a496cd41d919f0c6d51f6db984499
Yusuke Ogata <yusuke.ogata.yo@gmail.com>
2020-02-12 23:07:15 +0900

    [test] lesson#05 画面にappleの文字列を表示
    elem.textContent = 'text' を用いて、画面に文字列を表示。

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 'use strict';
 
 {
+  const word = 'apple';
+
+  const target = document.getElementById('target');
+  target.textContent = word;
   window.addEventListener('keydown', (e) => {
     console.log(e.key);
   });
