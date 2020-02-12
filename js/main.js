96752c45666906ca940e479d17198e6ce889db36
Yusuke Ogata <yusuke.ogata.yo@gmail.com>
2020-02-12 23:47:46 +0900

    [add] lesson#10 残り時間を少数2桁の秒数で表示
    num.toFixed(2)で小数点以下2桁で表示することができる。

--- js/main.js
+++ js/main.js
@@ -7,15 +7,19 @@
     'blue',
     'middle',
     'set',
-  ]
+  ];
+
   let word = words[Math.floor(Math.random() * words.length)];
   let loc = 0;
   let score = 0;
   let miss = 0;
+  const timeLimit = 3 * 1000;
+  let startTime;
 
   const target = document.getElementById('target');
   const scoreLabel = document.getElementById('score');
   const missLabel = document.getElementById('miss');
+  const timerLabel = document.getElementById('timer');
   
   function updateTarget() {
     let placeHolder = '';
@@ -25,8 +29,15 @@
     target.textContent = placeHolder + word.substring(loc);
   }
 
+  function updateTimer() {
+    const timeLeft = startTime + timeLimit - Date.now();
+    timerLabel.textContent = (timeLeft / 1000).toFixed(2);
+  }
+
   window.addEventListener('click', () => {
     target.textContent = word;
+    startTime = Date.now();
+    updateTimer();
   })
 
   window.addEventListener('keydown', (e) => {
