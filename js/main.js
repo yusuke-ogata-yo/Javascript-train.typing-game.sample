8dc91e141aa1fa968671ee8bb489e3c9e2c1b34e
Yusuke Ogata <yusuke.ogata.yo@gmail.com>
2020-02-12 23:19:17 +0900

    [test] lesson#06 入力が正解や不正解の数をカウントし画面に表示する機能の追加
    getElementByIdでelemetを取得し、elem.textContent = scoreでテキストを更新。

--- js/main.js
+++ js/main.js
@@ -3,16 +3,22 @@
 {
   const word = 'apple';
   let loc = 0;
+  let score = 0;
+  let miss = 0;
 
   const target = document.getElementById('target');
+  const scoreLabel = document.getElementById('score');
+  const missLabel = document.getElementById('miss');
   target.textContent = word;
   window.addEventListener('keydown', (e) => {
-    console.log(e.key);
     if (e.key === word[loc]){
-      console.log('score');
       loc++;
+      score++;
+      scoreLabel.textContent = score;
     } else {
-      console.log('miss');
+      miss++;
+      missLabel.textContent = miss;
+
     }
   });
 }
