f6505a6bde0182905b423e3f070c109248fba410
Yusuke Ogata <yusuke.ogata.yo@gmail.com>
2020-02-12 23:11:20 +0900

    [test] lesson#05 入力された文字が表示された文字とあっているか判定する機能の追加
    文字列の場所(location)の取得：word[loc]
    入力された文字と等しければ、locをインクリメント。

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@
 
 {
   const word = 'apple';
+  let loc = 0;
 
   const target = document.getElementById('target');
   target.textContent = word;
   window.addEventListener('keydown', (e) => {
     console.log(e.key);
+    if (e.key === word[loc]){
+      console.log('score');
+      loc++;
+    } else {
+      console.log('miss');
+    }
   });
 }
